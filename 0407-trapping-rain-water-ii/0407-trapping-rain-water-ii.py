a6450cbd330db8c405a793d00324cf3135e227b5
Akhilesh Aher <aherakhilesh88@gmail.com>
2025-01-19 14:39:02 +0530

    Time: 223 ms (17.86%), Space: 14.2 MB (7.14%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def trapRainWater(self, heightMap):
+        if not heightMap or not heightMap[0]:
+            return 0
+
+        m,n=len(heightMap),len(heightMap[0])
+        visited = [[False]* n for _ in range(m)]
+        minHeap = []
+
+        for i in range(m):
+            for j in range(n):
+                if i==0 or j==0 or i==m-1 or j==n-1:
+                    heapq.heappush(minHeap, (heightMap[i][j],i,j ))
+                    visited[i][j] =True
+
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        waterTrapped = 0
+        while minHeap:
+            height,x,y = heapq.heappop(minHeap)
+
+            for dx,dy in directions:
+                nx,ny = x+ dx, y+dy
+
+                if 0 <= nx <m and 0 <= ny <n and not visited[nx][ny]:
+                    visited[nx][ny] =True
+                    waterTrapped +=max(0,height - heightMap[nx][ny])
+                    heapq.heappush(minHeap,(max(height,heightMap[nx][ny]),nx,ny))
+
+        return waterTrapped 
+        
